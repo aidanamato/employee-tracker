0e3279da1a36319c64e723677573d0121ead694b
Aidan Amato <aidanamato@comcast.net>
2021-09-19 22:22:41 -0400

    Add post employee

--- server/routes/index.js
+++ server/routes/index.js
@@ -36,6 +36,23 @@ router.post('/department', ({body}, res) => {
 
 // role routes
 router.get('/roles', (req, res) => {
+  const sql = `SELECT roles.*, departments.name AS department
+               FROM roles
+               JOIN departments ON roles.department_id = departments.id`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(400).json({error: err.message});
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+router.get('/roles/internal', (req, res) => {
   const sql = 'SELECT * FROM roles';
 
   db.query(sql, (err, rows) => {
@@ -70,7 +87,13 @@ router.post('/role', ({body}, res) => {
 
 // employee routes
 router.get('/employees', (req, res) => {
-  const sql = `SELECT * FROM employees`;
+  const sql = `SELECT a.id, a.first_name, a.last_name, roles.title AS role, departments.name AS department, roles.salary, CONCAT(b.first_name, " ", b.last_name) AS manager, a.is_manager
+               FROM employees a LEFT JOIN employees b
+               ON a.manager_id = b.id
+               JOIN roles
+               ON a.role_id = roles.id
+               JOIN departments
+               ON roles.department_id = departments.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
